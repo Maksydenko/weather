9db4644b2673604b4de90f79ce7c09420d476415
Maksym Davydenko <maksydenko@gmail.com>
2023-06-07 16:55:01 +0300

    Minor fixes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,9 @@
 import type { AppProps } from "next/app";
+
 import { Provider } from "react-redux";
+
 import { store } from "@/provider/store";
+
 import "@/assets/scss/globals.scss";
 
 const App = ({ Component, pageProps }: AppProps) => (
