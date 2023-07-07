29fa476d4dbec4bd8e04546f577832f3b11230bb
Maksym Davydenko <maksydenko@gmail.com>
2023-07-07 19:41:04 +0300

    Minor weatherApi fix

--- src/provider/weather/weatherApi.ts
+++ src/provider/weather/weatherApi.ts
@@ -1,13 +1,13 @@
 import { IWeather } from "@/interfaces/weather.interface";
 
-const API_KEY = "b325a710045095ee58c4b40ab240b272";
-const API_URL = "https://api.openweathermap.org/data/2.5/weather";
-
 interface IFetchWeatherData {
   (location: string): Promise<IWeather>;
 }
 
 export const fetchWeatherData: IFetchWeatherData = async (location: string) => {
+  const API_KEY = "b325a710045095ee58c4b40ab240b272";
+  const API_URL = "https://api.openweathermap.org/data/2.5/weather";
+
   const response = await fetch(
     `${API_URL}?q=${location}&&appid=${API_KEY}&units=metric`
   );
