b1a7a0df8104da6c34ed2dac08207f8d4cb00c7e
Maksym Davydenko <maksydenko@gmail.com>
2023-08-15 18:28:23 +0300

    FullScreen fix

--- src/components/base/FullScreen/FullScreen.tsx
+++ src/components/base/FullScreen/FullScreen.tsx
@@ -1,7 +1,5 @@
 import { FC, ReactNode } from "react";
 
-import { useFullHeight } from "./useFullHeight";
-
 import { handleClassName } from "@/utils/className.util";
 
 interface FullScreenProps {
@@ -17,20 +15,14 @@ const FullScreen: FC<FullScreenProps> = ({
   children,
   background,
 }) => {
-  const height = useFullHeight();
-
   const modifiedClassName = handleClassName(
     !!modifier,
     `${className}__full-screen`,
     modifier
   );
 
-  const style = {
-    minHeight: height,
-  };
-
   return (
-    <div className={`${modifiedClassName} full-screen`} style={style}>
+    <div className={`${modifiedClassName} full-screen`}>
       <div className="full-screen__body">{children}</div>
       {background}
     </div>
